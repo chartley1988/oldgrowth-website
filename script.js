c817b41dfffd7a74c6334e287f1c81231d2b700e
Carson Hartley <chartley1988@gmail.com>
2022-05-25 15:49:06 -0600

    Fixed the spacing in the nav-bar, updated some content.

---
+++ script.js
@@ -0,0 +1,11 @@
+let carson = "Awesome!"
+
+window.addEventListener('load', (event) => {
+    console.log("Carson is " + carson);
+  });
+
+console.log("Huh? Does this display?")
+
+document.addEventListener('click', (event) => {
+    console.log("Oh baby! a click!");
+});
